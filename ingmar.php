4ec6e991435a0a5e3a56983c6ddd13939a236bf2
David Hewitson <dwhewitson@gmail.com>
2016-03-18 13:56:53 +0000

    init

---
+++ ingmar.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Ingmar
+ * Version: 0.1-alpha
+ * Description: Modern development tools for WordPress
+ * Author: David Hewitson
+ * Author URI: http://github.com/shstkvch/
+ * Plugin URI: http://github.com/shstkvch/ingmar
+ * Text Domain: ingmar
+ * Domain Path: /languages
+ * @package Ingmar
+ */
